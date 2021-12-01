d7240e3a5974cd559fa5503a9c362f3d69624c23
shabnam rani <88382451+thakurshabnam231@users.noreply.github.com>
2021-12-01 08:02:26 -0800

    upload files to aws s3

---
+++ src/components/Upload.js
@@ -0,0 +1,38 @@
+import React , {useState} from 'react';
+import { uploadFile } from 'react-s3';
+
+
+const S3_BUCKET ='reactjs-shabnam';
+const REGION ='ap-south-1';
+const ACCESS_KEY ='AKIATZUNEARHBLL2U54H';
+const SECRET_ACCESS_KEY ='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';
+
+const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
+
+const Upload= () => {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    const handleUpload = async (file) => {
+        uploadFile('http://reactjs-shabnam.s3-website.ap-south-1.amazonaws.com', config)
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
+    }
+
+    return <div>
+        <div>React S3 File Upload</div>
+        <input type="file" onChange={handleFileInput}/><br/>
+        <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+    </div>
+}
+
+export default Upload;
